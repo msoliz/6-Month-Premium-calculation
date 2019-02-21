dfcc496ea48677d79d0cd0066984c2994c65b55d
Mark Soliz <mrmarksoliz@gmail.com>
2019-02-21 17:55:32 -0600

    added total 6 month term premium

--- 6month.js
+++ 6month.js
@@ -1,10 +1,8 @@
 //project should display 6 month premium per vehicle
-//it should get amount of vehicles ?? just enter amounts into array
-
-//it should get 6month term premium of all cars in array
-//it should get 6month term premium and divide per 6 -- per vehicle
-//if difference is positive, divide remainder between total vehicles and then add per veh
-//if diff negative, divide remainder between veh and the subtract per veh
+//it should get sum of all cars in array
+//
+//if sum is positive, divide remainder between total vehicles and then add per veh
+//if sum is negative, divide remainder between veh and the subtract per veh
 
 //display new amounts per veh and verify the add up to monthly premium
 
@@ -14,32 +12,31 @@
 // var months = 6;
 var premium = [];
 var sum = 0;
-
 function pushFunction(){
     
     premium.push(document.getElementById('premiumInput').value);
     premiumInput.value = '';
-    sum = premium.reduce(sumFunction);
-
-    console.log(premium);
 }
+
 
 function calculatePremium() {
-
+    if (premium.length === 0){
+        console.log('No amount has been entered');
+    }   else{
+        for (var i = 0; i > premium.length; i++){
+                sum = premium.reduce( function(total, amount){
+                return total + amount;
+                
+            });
+        }
+        console.log(sum);
+    } 
+    
 }
 
-function sumFunction(total, value)  {
-    return total + value;
-}
 
-// if (premium.length === 0){
-//     console.log('no monies have been entered');
-// }   else{
-//     for (var i = 0; i > premium.length; i++){
-//         var newPremium = premium[i] / months;
-//         console.log(newPremium); 
-//     }
-// }    
+
+   
 
 
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# 6-Month-Premium-calculation
